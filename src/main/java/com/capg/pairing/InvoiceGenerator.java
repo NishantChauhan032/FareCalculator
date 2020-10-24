9dde8ed31bd0f966482153e5e2108cb6f6f19486
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-24 09:34:14 +0530

    [Mukesh] add : Ability to calculate fare for a ride

--- src/main/java/com/capg/pairing/InvoiceGenerator.java
+++ src/main/java/com/capg/pairing/InvoiceGenerator.java
@@ -2,8 +2,10 @@ package com.capg.pairing;
 
 public class InvoiceGenerator 
 {
+	private static final int DISTANCE_RATE = 10;
+	private static final int TIME_RATE = 1;
+	
 	public double calculateFare(double distance, int time) {
-		
-		return 0;
+		return distance*DISTANCE_RATE + time * TIME_RATE;
 	}
 }
--- src/test/java/com/capg/pairing/InvoiceServiceTest.java
+++ src/test/java/com/capg/pairing/InvoiceServiceTest.java
@@ -1,17 +1,16 @@
 package com.capg.pairing;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-import org.junit.Assert;
-
-public class InvoiceServiceTest 
-{
- @Test
- public void givenDistanceAndTime_whenCalculated_shouldReturnTotalFare() {
-	 InvoiceGenerator fareCalculator = new InvoiceGenerator();
-	 double distance = 4.0 ;
-	 int time = 10;
-	 double fare = fareCalculator.calculateFare(distance,time);
-	 Assert.assertEquals(50,fare);
- }
+public class InvoiceServiceTest {
+	@Test
+	public void givenDistanceAndTime_whenCalculated_shouldReturnTotalFare() {
+		InvoiceGenerator fareCalculator = new InvoiceGenerator();
+		double distance = 4.0;
+		int time = 10;
+		double fare = fareCalculator.calculateFare(distance, time);
+		assertEquals(50, fare, 0);
+	}
 }
