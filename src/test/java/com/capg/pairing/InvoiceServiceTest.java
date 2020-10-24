faa252a0ba1d10b8848a0855c879915dc97c9dd7
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-24 13:06:41 +0530

    [Mukesh] add : Ability to generate Invoice Summary

--- src/test/java/com/capg/pairing/InvoiceServiceTest.java
+++ src/test/java/com/capg/pairing/InvoiceServiceTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
 	InvoiceGenerator invoiceGenerator = null;
-	
+
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		invoiceGenerator = new InvoiceGenerator();
 	}
-	
+
 	@Test
 	public void givenDistanceAndTime_whenCalculated_shouldReturnTotalFare() {
 		double distance = 4.0;
@@ -27,13 +27,15 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
 	}
-	
+
 	@Test
 	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-		Ride[] rides = {new Ride(4.0, 10),
-						new Ride(0.1, 1)};
+		Ride[] rides = { 
+						new Ride(4.0, 10), 
+						new Ride(0.1, 1) 
+					};
 		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
-		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,50,0);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 55);
 		Assert.assertEquals(expectedInvoiceSummary, summary);
 	}
 }
