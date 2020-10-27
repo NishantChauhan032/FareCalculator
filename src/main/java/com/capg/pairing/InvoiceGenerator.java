6a6ace0fe86db975bf79e9ffe514939320d0a250
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-27 22:02:49 +0530

    [Mukesh] edit : edited to match the new requirements of noraml and premium customer

--- src/main/java/com/capg/pairing/InvoiceGenerator.java
+++ src/main/java/com/capg/pairing/InvoiceGenerator.java
@@ -1,28 +1,18 @@
 package com.capg.pairing;
 
-import java.util.List;
-
 public class InvoiceGenerator {
-	private static final int DISTANCE_RATE = 10;
-	private static final int TIME_RATE = 1;
-	private static final int MINIMUM_FARE = 5;
 
-	RideRepository rideRepository = null;
+	private RideRepository rideRepository = null;
 
-	public InvoiceGenerator() {
-		rideRepository = new RideRepository();
-	}
-
-	public double calculateFare(double distance, int time) {
-		double totalFare = distance * DISTANCE_RATE + time * TIME_RATE;
-		return Math.max(totalFare, MINIMUM_FARE);
+	public void setRideRepository(RideRepository rideRepository) {
+		this.rideRepository = rideRepository;
 	}
 
 	public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0;
 
 		for (Ride ride : rides) {
-			totalFare += this.calculateFare(ride.distance, ride.time);
+			totalFare += ride.cabRide.calculateFare(ride);
 		}
 		return new InvoiceSummary(rides.length, totalFare);
 	}
--- src/main/java/com/capg/pairing/Ride.java
+++ src/main/java/com/capg/pairing/Ride.java
@@ -3,9 +3,11 @@ package com.capg.pairing;
 public class Ride {
 	public double distance;
 	public final int time;
+	public CabRide cabRide;
 
-	public Ride(double distance, int time) {
+	public Ride(CabRide cabRide, double distance, int time) {
 		this.distance = distance;
 		this.time = time;
+		this.cabRide = cabRide;
 	}
 }
